fd5af2b0669b44fce216a252afba4bed27cd6bc8
Grace Tantichatwat <graceetantii@gmail.com>
2026-04-22 09:57:32 +1000

    Fix linting errors in Octocat class
    Refactor Octocat class for better linting compliance.

--- myfile.py
+++ myfile.py
@@ -1,13 +1,14 @@
-# A sample Python file with some linting errors for ruff/flake8 to find
+# A sample Python file with some linting errors for ruff to find
 
 class Octocat:
-    def __init__(self, name, breeds):
-        self.name = name
-        self.breeds = breeds
+    def __init__(self, name, breeds):
+        self.name = name
+        self.breeds = breeds
 
-    def display(self):
-        breed = "-".join(self.breeds)
-        print(f"I am of {breed} breed, and my name is {self.name}.")
+    def display(self):
+        breed = "-".join(self.breeds)
+        unused_variable = "this will trigger a linter warning"
+        print(f"I am of {breed} breed, and my name is {self.name}.")
 
 m = Octocat("Mona", ["cat", "octopus"])
 m.display()
